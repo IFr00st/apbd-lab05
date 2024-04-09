ca2bf087ec1f9c8edd5c58da8d4a3b1005a96d20
s27885 <s27885@pjwstk.edu.pl>
2024-04-09 14:36:27 +0200

    add cos minimal api

--- cos/lab05/lab05/Program.cs
+++ cos/lab05/lab05/Program.cs
@@ -16,29 +16,22 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+//Minimal api
+
+app.MapGet("/animals", () =>
+{
+   // 200 - Ok
+   // 400 - Bad request
+   // 404 - Not Found
+   // 401 - Unauthorized
+    return Results.Ok(); 
+});
+app.MapGet("/animals", () =>
+{
+
+});
+
+
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
